7ca92f715c790bfffcbfd5853376b79691353514
BSKOM <b.shelestua@gmail.com>
2021-08-04 18:03:07 +0300

    fix:fix getWinner

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,22 +1,24 @@
 class TicTacToe {
   constructor() {
     this.player = 'x',
-    this.matrix = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ],
+      this.matrix = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ],
 
-    this.win = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-      [1, 5, 9],
-      [3, 5, 7]
-    ]
+      this.win = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+      ],
+
+      this.finised = false
 
   }
 
@@ -33,34 +35,31 @@ class TicTacToe {
   }
 
   isFinished() {
-    console.log('isFinished()', this.player)
-    if (this.getWinner() || this.isDraw()) {
+    if (this.finished) {
       return true
     }
     return false
   }
 
   getWinner() {
-    console.log('getwinner', this.matrix, this.player)
     let curPlayer = this.player
     let arr = []
     for (let i = 0; i < 9; i++) {
-      arr[i] = this.matrix[Math.floor((i) / 3)][i % 3] === 'x' ? i + 1 : 0
+      arr[i] = this.matrix[Math.floor((i) / 3)][i % 3] === curPlayer ? i + 1 : 0
     }
     for (let i = 0; i < this.win.length; i++) {
       if (this.win[i].every(item => arr.indexOf(item) !== -1)) {
-        console.log('win1', curPlayer)
+        this.finised = true
         return curPlayer
       }
     }
     curPlayer = curPlayer==='x'?'o':'x'
     arr = []
     for (let i = 0; i < 9; i++) {
-      arr[i] = this.matrix[Math.floor((i) / 3)][i % 3] === 'x' ? i + 1 : 0
+      arr[i] = this.matrix[Math.floor((i) / 3)][i % 3] === curPlayer ? i + 1 : 0
     }
     for (let i = 0; i < this.win.length; i++) {
       if (this.win[i].every(item => arr.indexOf(item) !== -1)) {
-        console.log('win2',curPlayer)
         return curPlayer
       }
     }
@@ -70,22 +69,23 @@ class TicTacToe {
   noMoreTurns() {
     // console.log('noMoreTurns', this.matrix.flat(1),  this.player)
     if (this.matrix.flat(1).indexOf(null) === -1) {
-      console.log('yes! noMoreTurns')
+      // console.log('yes! noMoreTurns')
       return true
     }
     return false
   }
 
   isDraw() {
-    console.log('isDraw', this.player)
+    // console.log('isDraw', this.player)
     if (this.noMoreTurns() && !this.getWinner()) {
-      return true   
+      this.finished = true
+      return true
     }
     return false
   }
 
   getFieldValue(rowIndex, colIndex) {
-    console.log('getFieldValue', rowIndex, colIndex, this.player)
+    // console.log('getFieldValue', rowIndex, colIndex, this.player)
     return this.matrix[rowIndex][colIndex]
   }
 }
