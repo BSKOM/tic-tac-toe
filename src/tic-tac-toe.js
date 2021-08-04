fd2e9e387a4b1db9495975b03e239757e7b6bbae
BSKOM <b.shelestua@gmail.com>
2021-08-04 15:46:07 +0300

    feat: create prototype tic-tac-toe

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,79 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.player = 'x',
+    this.matrix = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ],
 
+    this.win = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ]
+
+  }
+
+  getCurrentPlayerSymbol() {
+    // console.log('getCurrentPlayerSymbol', this.player )
+    return this.player
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    // console.log('nextTurn()', rowIndex, columnIndex,this.player)
+    if (this.matrix[rowIndex][columnIndex]) return null
+    this.matrix[rowIndex][columnIndex] = this.player
+    this.player = this.player === 'x' ? 'o' : 'x'
+  }
+
+  isFinished() {
+    console.log('isFinished()', this.player)
+    if (this.getWinner() || this.isDraw()) {
+      return true
     }
+    return false
+  }
 
-    getCurrentPlayerSymbol() {
-
+  getWinner() {
+    console.log('getwinner', this.player)
+    let arr = []
+    for (let i = 0; i < 9; i++) {
+      arr[i] = this.matrix[math.floor((i) / 3)][i % 3] === this.player ? i + 1 : 0
     }
-
-    nextTurn(rowIndex, columnIndex) {
-
+    for (let i = 0; j < this.win.length(); i++) {
+      if (this.win[i].every(item => arr.indexOf(item) !== -1)) {
+        return this.player
+      }
     }
+    return false
+  }
 
-    isFinished() {
-
+  noMoreTurns() {
+    console.log('noMoreTurns', this.player)
+    if (this.matrix.flat(1).indexOf(null) === -1) {
+      return true
     }
+    return false
+  }
 
-    getWinner() {
-
+  isDraw() {
+    console.log('isDraw', this.player)
+    if (!this.noMoreTurns() || this.getWinner()) {
+      return false
     }
+    return true
+  }
 
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    console.log('getFieldValue', rowIndex, colIndex, this.player)
+    return this.matrix[rowIndex][colIndex]
+  }
 }
 
 module.exports = TicTacToe;
